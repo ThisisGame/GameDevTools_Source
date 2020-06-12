29e02a4bdde8e5357a8ea277af7a3436f5c26916
captain <790621656@qq.com>
2020-06-12 11:26:55 +0800

    EncryptResources_Xor

---
+++ EncryptResources_Xor/EncryptResources/EncryptResources/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace EncryptResources
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.textBoxEncryptKey = new System.Windows.Forms.TextBox();
+            this.buttonChooseSrc = new System.Windows.Forms.Button();
+            this.labelChooseSrc = new System.Windows.Forms.Label();
+            this.labelChooseDst = new System.Windows.Forms.Label();
+            this.buttonChooseDst = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(37, 198);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 0;
+            this.buttonEncrypt.Text = "加密";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // textBoxEncryptKey
+            // 
+            this.textBoxEncryptKey.Location = new System.Drawing.Point(37, 152);
+            this.textBoxEncryptKey.Name = "textBoxEncryptKey";
+            this.textBoxEncryptKey.Size = new System.Drawing.Size(134, 21);
+            this.textBoxEncryptKey.TabIndex = 1;
+            this.textBoxEncryptKey.Text = "输入整数(1-254)密钥";
+            // 
+            // buttonChooseSrc
+            // 
+            this.buttonChooseSrc.Location = new System.Drawing.Point(37, 30);
+            this.buttonChooseSrc.Name = "buttonChooseSrc";
+            this.buttonChooseSrc.Size = new System.Drawing.Size(134, 23);
+            this.buttonChooseSrc.TabIndex = 2;
+            this.buttonChooseSrc.Text = "选择需要加密的文件夹";
+            this.buttonChooseSrc.UseVisualStyleBackColor = true;
+            this.buttonChooseSrc.Click += new System.EventHandler(this.buttonChooseSrc_Click);
+            // 
+            // labelChooseSrc
+            // 
+            this.labelChooseSrc.AutoSize = true;
+            this.labelChooseSrc.Location = new System.Drawing.Point(37, 60);
+            this.labelChooseSrc.Name = "labelChooseSrc";
+            this.labelChooseSrc.Size = new System.Drawing.Size(41, 12);
+            this.labelChooseSrc.TabIndex = 3;
+            this.labelChooseSrc.Text = "label1";
+            // 
+            // labelChooseDst
+            // 
+            this.labelChooseDst.AutoSize = true;
+            this.labelChooseDst.Location = new System.Drawing.Point(39, 120);
+            this.labelChooseDst.Name = "labelChooseDst";
+            this.labelChooseDst.Size = new System.Drawing.Size(41, 12);
+            this.labelChooseDst.TabIndex = 5;
+            this.labelChooseDst.Text = "label2";
+            // 
+            // buttonChooseDst
+            // 
+            this.buttonChooseDst.Location = new System.Drawing.Point(39, 90);
+            this.buttonChooseDst.Name = "buttonChooseDst";
+            this.buttonChooseDst.Size = new System.Drawing.Size(134, 23);
+            this.buttonChooseDst.TabIndex = 4;
+            this.buttonChooseDst.Text = "选择存放文件夹";
+            this.buttonChooseDst.UseVisualStyleBackColor = true;
+            this.buttonChooseDst.Click += new System.EventHandler(this.buttonChooseDst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 306);
+            this.Controls.Add(this.labelChooseDst);
+            this.Controls.Add(this.buttonChooseDst);
+            this.Controls.Add(this.labelChooseSrc);
+            this.Controls.Add(this.buttonChooseSrc);
+            this.Controls.Add(this.textBoxEncryptKey);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.TextBox textBoxEncryptKey;
+        private System.Windows.Forms.Button buttonChooseSrc;
+        private System.Windows.Forms.Label labelChooseSrc;
+        private System.Windows.Forms.Label labelChooseDst;
+        private System.Windows.Forms.Button buttonChooseDst;
+    }
+}
+
---
+++ EncryptResources_Xor/EncryptResources/EncryptResources/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptResources
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string mChooseSrc = string.Empty;
+        private string mChooseDst = string.Empty;
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            //先判断密钥对不对
+            string tmpEncryptStr = textBoxEncryptKey.Text;
+            int tmpEncryptKey = 0;
+            if (int.TryParse(tmpEncryptStr, out tmpEncryptKey) == false)
+            {
+                MessageBox.Show("错误", "密钥输入错误 整数 且 大于0 小于255");
+                return;
+            }
+
+            if (tmpEncryptKey <= 0 || tmpEncryptKey >= 255)
+            {
+                MessageBox.Show("错误", "密钥输入错误 整数 且 大于0 小于255");
+                return;
+            }
+
+
+            Encrypt((byte)tmpEncryptKey);
+        }
+
+        private void Encrypt(byte varEncryptKey)
+        {
+            mChooseSrc= mChooseSrc.Replace("\\", "/");
+            string[] bundlefiles = Directory.GetFiles(mChooseSrc, "*.*", SearchOption.AllDirectories);
+            for (int i = 0; i < bundlefiles.Length; i++)
+            {
+                string tmpfilepath = bundlefiles[i];
+
+                string tmpEncryptFilePath = tmpfilepath.Replace("\\", "/");
+ 
+                tmpEncryptFilePath = tmpEncryptFilePath.Replace(mChooseSrc, mChooseDst); //Windows
+
+                byte[] tmpData = File.ReadAllBytes(tmpfilepath);
+
+                for (int tmpDataIndex = 0; tmpDataIndex < tmpData.Length; tmpDataIndex++)
+                {
+                    //加密
+                    tmpData[tmpDataIndex] ^= varEncryptKey;
+                }
+
+
+                string tmpEncryptFileDirectoryPath = Path.GetDirectoryName(tmpEncryptFilePath);
+
+
+
+                if (Directory.Exists(tmpEncryptFileDirectoryPath) == false)
+                {
+
+                    Directory.CreateDirectory(tmpEncryptFileDirectoryPath);
+                }
+                File.WriteAllBytes(tmpEncryptFilePath, tmpData);
+            }
+
+            //写入EncryptKey
+            File.WriteAllText(mChooseDst + "/EncryptKey", varEncryptKey.ToString());
+        }
+
+        private void buttonChooseSrc_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择需要加密的目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                mChooseSrc = folder.SelectedPath;
+                labelChooseSrc.Text = mChooseSrc;
+            }
+        }
+
+        private void buttonChooseDst_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择需要加密的目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                mChooseDst = folder.SelectedPath;
+                labelChooseDst.Text = mChooseDst;
+            }
+        }
+    }
+}
