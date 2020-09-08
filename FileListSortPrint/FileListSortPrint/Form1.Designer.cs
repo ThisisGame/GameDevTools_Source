c780309fca5c2b087835bafbe52e41e0c119b2b7
captain <790621656@qq.com>
2020-09-08 20:59:14 +0800

    计数排序

--- FileListSortPrint/FileListSortPrint/Form1.Designer.cs
+++ FileListSortPrint/FileListSortPrint/Form1.Designer.cs
@@ -28,19 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.buttonChooseDir = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSortStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.radioButtonBubbleSort = new System.Windows.Forms.RadioButton();
+            this.radioButtonCountingSort = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonChooseDir
             // 
-            this.button1.Location = new System.Drawing.Point(92, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "选择文件夹";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonChooseDir.Location = new System.Drawing.Point(4, 142);
+            this.buttonChooseDir.Name = "buttonChooseDir";
+            this.buttonChooseDir.Size = new System.Drawing.Size(152, 41);
+            this.buttonChooseDir.TabIndex = 0;
+            this.buttonChooseDir.Text = "选择文件夹";
+            this.buttonChooseDir.UseVisualStyleBackColor = true;
+            this.buttonChooseDir.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -48,18 +54,70 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(241, 72);
+            this.textBox1.Size = new System.Drawing.Size(317, 72);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "点击选择文件夹，生成选定目录文件列表，并按文件大小排序，从大到小。";
+            // 
+            // buttonSortStart
+            // 
+            this.buttonSortStart.Location = new System.Drawing.Point(162, 142);
+            this.buttonSortStart.Name = "buttonSortStart";
+            this.buttonSortStart.Size = new System.Drawing.Size(159, 41);
+            this.buttonSortStart.TabIndex = 3;
+            this.buttonSortStart.Text = "开始";
+            this.buttonSortStart.UseVisualStyleBackColor = true;
+            this.buttonSortStart.Click += new System.EventHandler(this.buttonSortStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.progressBar1.Location = new System.Drawing.Point(4, 189);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(317, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // radioButtonBubbleSort
+            // 
+            this.radioButtonBubbleSort.AutoSize = true;
+            this.radioButtonBubbleSort.Checked = true;
+            this.radioButtonBubbleSort.Location = new System.Drawing.Point(4, 90);
+            this.radioButtonBubbleSort.Name = "radioButtonBubbleSort";
+            this.radioButtonBubbleSort.Size = new System.Drawing.Size(191, 16);
+            this.radioButtonBubbleSort.TabIndex = 5;
+            this.radioButtonBubbleSort.TabStop = true;
+            this.radioButtonBubbleSort.Text = "冒泡排序(超慢但占用最少内存)";
+            this.radioButtonBubbleSort.UseVisualStyleBackColor = true;
+            this.radioButtonBubbleSort.CheckedChanged += new System.EventHandler(this.radioButtonBubbleSort_CheckedChanged);
+            // 
+            // radioButtonCountingSort
+            // 
+            this.radioButtonCountingSort.AutoSize = true;
+            this.radioButtonCountingSort.Location = new System.Drawing.Point(4, 112);
+            this.radioButtonCountingSort.Name = "radioButtonCountingSort";
+            this.radioButtonCountingSort.Size = new System.Drawing.Size(317, 16);
+            this.radioButtonCountingSort.TabIndex = 6;
+            this.radioButtonCountingSort.Text = "计数排序(极速但占用内存，消耗内存=最大文件字节数)";
+            this.radioButtonCountingSort.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(248, 128);
+            this.ClientSize = new System.Drawing.Size(325, 220);
+            this.Controls.Add(this.radioButtonCountingSort);
+            this.Controls.Add(this.radioButtonBubbleSort);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonSortStart);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonChooseDir);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "文件列表排序";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,8 +126,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonChooseDir;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSortStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RadioButton radioButtonBubbleSort;
+        private System.Windows.Forms.RadioButton radioButtonCountingSort;
     }
 }
 
--- FileListSortPrint/FileListSortPrint/Form1.cs
+++ FileListSortPrint/FileListSortPrint/Form1.cs
@@ -7,11 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace FileListSortPrint
 {
     public partial class Form1 : Form
     {
+        string chooseDirPath = string.Empty;
+        Dictionary<string, FileInfo> keyValuePairs = new Dictionary<string, FileInfo>();
+
+        string logStr = string.Empty;
+
+        bool working = false;
+
+        Queue<string> logQueue = new Queue<string>();
+
+        int progress = 0;
+
+        long startTimeStamp = 0;
+
+        enum SortAlgorithm
+        {
+            /// <summary>
+            /// 冒泡
+            /// </summary>
+            BubbleSort,
+            /// <summary>
+            /// 计数排序
+            /// </summary>
+            CountingSort
+        }
+        SortAlgorithm sortAlgorithm =SortAlgorithm.BubbleSort ;//冒泡
+
         public Form1()
         {
             InitializeComponent();
@@ -32,52 +60,259 @@ namespace FileListSortPrint
             return fileInfo;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Log(string str)
         {
-            string savePath = null;
-            FolderBrowserDialog dialog = new FolderBrowserDialog();
-            dialog.Description = "请选择文件夹";
+            logQueue.Enqueue(str);
+            //Console.Write(str);
+        }
 
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                savePath = dialog.SelectedPath;
-            }
-            if(savePath==null)
-            {
-                return;
-            }
+        private long GetUnixTimeStamp()
+        {
+            return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+        }
 
-            List<string> fileList = new List<string>();
-            Dictionary<string, FileInfo> keyValuePairs = new Dictionary<string, FileInfo>();
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="files"></param>
+        private void BubbleSort(string[] files)
+        {
+            Log("BubbleSort");
+            int lastpercent = -1;
 
-            string[] files= Directory.GetFiles(savePath, "*.*", SearchOption.AllDirectories);
-
-            //排序
             for (int i = 0; i < files.Length; i++)
             {
-                for (int j = 0; j < files.Length-i-1; j++)//每次从左边开始，一次内循环挑出最大的放到最右边
+                for (int j = 0; j < files.Length - i - 1; j++)//每次从左边开始，一次内循环挑出最大的放到最右边
                 {
 
-                    FileInfo fileInfo= GetFileInfo(keyValuePairs, files[j]);
+                    FileInfo fileInfo = GetFileInfo(keyValuePairs, files[j]);
                     FileInfo fileInfoRight = GetFileInfo(keyValuePairs, files[j + 1]);
 
-                    if (fileInfo.Length>fileInfoRight.Length)
+                    if (fileInfo.Length > fileInfoRight.Length)
                     {
                         string tmp = files[j + 1];
                         files[j + 1] = files[j];
                         files[j] = tmp;
                     }
                 }
+
+                int percent = i * 100 / files.Length;
+
+                progress = percent;
+
+                if (percent!=lastpercent && percent%5==0)
+                {
+                    Log("BubbleSort: " + i + "/" + files.Length+" 进度:"+ percent+"%");
+
+                    lastpercent = percent;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 计数排序
+        /// </summary>
+        /// <param name="files"></param>
+        private void CountingSort(string[] files)
+        {
+            Log("CountingSort");
+            int lastpercent = -1;
+
+            long maxFileSize = 0;
+
+            //以文件大小作为key 相同大小的文件名 存放在value的list里面。
+            Dictionary<long, List<string>> fileSize_fileNames = new Dictionary<long, List<string>>();
+
+            //先遍历一次，找出最大的文件,并且以文件大小作为key，进行分类。
+            for (int i = 0; i < files.Length; i++)
+            {
+                string filePath = files[i];
+                FileInfo fileInfo = new FileInfo(filePath);
+                long fileSize = fileInfo.Length;
+
+                List<string> fileNames = null;
+                if(fileSize_fileNames.ContainsKey(fileSize)==false)
+                {
+                    fileNames = new List<string>();
+                    fileSize_fileNames.Add(fileSize, fileNames);
+
+                    if(maxFileSize<fileSize)
+                    {
+                        maxFileSize = fileSize;
+                    }
+                }
+                else
+                {
+                    fileNames = fileSize_fileNames[fileSize];
+                }
+                fileNames.Add(filePath);
+
+                //更新进度
+                int percent = i * 100 / files.Length;
+
+                progress = percent;
+
+                if (percent != lastpercent && percent % 5 == 0)
+                {
+                    Log("CountingSort 统计文件字节大小: " + i + "/" + files.Length + " 进度:" + percent + "%");
+
+                    lastpercent = percent;
+                } 
             }
 
+            //以最大的文件尺寸，创建数组
+            int[] coutingArray = new int[maxFileSize+1];
+
+            //填入计数
+            foreach (var item in fileSize_fileNames)
+            {
+                coutingArray[item.Key] = item.Value.Count;
+
+            }
+
+            //coutingArray已经是有序的了。现在只要填入files就可以。
+            int fileIndex = 0;
+            for (int i = 0; i < coutingArray.Length; i++)
+            {
+                int fileSize = i;
+
+                if(fileSize_fileNames.ContainsKey(fileSize))
+                {
+                    List<string> fileNames = fileSize_fileNames[fileSize];
+
+                    foreach (var fileName in fileNames)
+                    {
+                        files[fileIndex++] = fileName;
+                    }
+                }
+
+                
+
+                //更新进度
+                int percent = i * 100 / coutingArray.Length;
+
+                progress = percent;
+
+                if (percent != lastpercent && percent % 5 == 0)
+                {
+                    Log("CountingSort 填充: " + i + "/" + files.Length + " 进度:" + percent + "%");
+
+                    lastpercent = percent;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 对目录文件进行排序
+        /// </summary>
+        /// <param name="state"></param>
+        private void SortFiles(Object state)
+        {
+            string dirPath = (string)state;
+
+            Log("搜索目录："+ dirPath);
+            string[] files = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+
+            Log("文件数量：" + files.Length);
+
+            startTimeStamp = GetUnixTimeStamp();
+
+            //排序
+            switch (sortAlgorithm)
+            {
+                case SortAlgorithm.BubbleSort:
+                    BubbleSort(files);
+                    break;
+                case SortAlgorithm.CountingSort:
+                    CountingSort(files);
+                    break;
+                default:
+                    break;
+            }
+
+            long costTime = GetUnixTimeStamp() - startTimeStamp;
+            Log("排序耗时:" + costTime);
+
+            //Log("写入txt");
             //保存到txt
             StreamWriter streamWriter = new StreamWriter("./files.txt");
-            for (int i = files.Length-1; i >=0; i--)
+            for (int i = files.Length - 1; i >= 0; i--)
             {
                 FileInfo fileInfo = GetFileInfo(keyValuePairs, files[i]);
-                streamWriter.WriteLine(fileInfo.Length+" "+files[i]);
+
+                streamWriter.WriteLine(fileInfo.Length + " " + files[i]);
             }
             streamWriter.Close();
+
+            
+            
+            working = false;
+            startTimeStamp = 0;
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (working)
+            {
+                return;
+            }
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                chooseDirPath = dialog.SelectedPath;
+
+                textBox1.Text ="目标文件夹:"+ chooseDirPath;
+            }
+        }
+
+        private void buttonSortStart_Click(object sender, EventArgs e)
+        {
+            if (working)
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(chooseDirPath))
+            {
+                MessageBox.Show("请先选择目录");
+                return;
+            }
+
+            if(radioButtonBubbleSort.Checked)
+            {
+                sortAlgorithm = SortAlgorithm.BubbleSort;
+            }
+            else if(radioButtonCountingSort.Checked)
+            {
+                sortAlgorithm = SortAlgorithm.CountingSort;
+            }
+
+            working = true;
+
+            Log("开启子线程执行");
+
+            ThreadPool.QueueUserWorkItem(SortFiles, chooseDirPath);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(logQueue.Count>0)
+            {
+                textBox1.Text = logQueue.Dequeue();
+                //Console.WriteLine("timer1_Tick");
+            }
+
+            progressBar1.Value = progress;
+        }
+
+        private void radioButtonBubbleSort_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
