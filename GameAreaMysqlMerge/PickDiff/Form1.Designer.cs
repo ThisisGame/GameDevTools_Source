370fc359321f8867eac267798976616d75991a4f
captain <790621656@qq.com>
2020-09-11 01:44:42 +0800

    通用mysql合服工具

---
+++ GameAreaMysqlMerge/PickDiff/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PickDiff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonPick = new System.Windows.Forms.Button();
+            this.buttonCreateDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择旧文件夹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(220, 58);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 83);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(220, 58);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "选择新文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonPick
+            // 
+            this.buttonPick.Location = new System.Drawing.Point(239, 152);
+            this.buttonPick.Name = "buttonPick";
+            this.buttonPick.Size = new System.Drawing.Size(113, 49);
+            this.buttonPick.TabIndex = 4;
+            this.buttonPick.Text = "生成";
+            this.buttonPick.UseVisualStyleBackColor = true;
+            this.buttonPick.Click += new System.EventHandler(this.buttonPick_Click);
+            // 
+            // buttonCreateDB
+            // 
+            this.buttonCreateDB.Location = new System.Drawing.Point(13, 220);
+            this.buttonCreateDB.Name = "buttonCreateDB";
+            this.buttonCreateDB.Size = new System.Drawing.Size(113, 49);
+            this.buttonCreateDB.TabIndex = 5;
+            this.buttonCreateDB.Text = "创建数据库";
+            this.buttonCreateDB.UseVisualStyleBackColor = true;
+            this.buttonCreateDB.Click += new System.EventHandler(this.buttonCreateDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 308);
+            this.Controls.Add(this.buttonCreateDB);
+            this.Controls.Add(this.buttonPick);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "对比2个文件夹，选出不同文件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonPick;
+        private System.Windows.Forms.Button buttonCreateDB;
+    }
+}
+
